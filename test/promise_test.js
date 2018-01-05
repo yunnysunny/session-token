81bf49bb15cde6eae3f900a83212629bdaccc3b0
yunnysunny <yunnysunny@gmail.com>
2018-01-05 23:15:09 +0800

    remove unused test file

--- test/promise_test.js
+++
@@ -1,8 +0,0 @@
-const Q = require('q');
-
-var deferred = Q.defer();
-const promise = deferred.promise;
-setTimeout(deferred.resolve, 1000);
-promise.then(function() {
-    console.log('promise resolved.');
-});
