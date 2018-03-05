dd13a8fbc9d8e54e33a928dba13678454ea754c3
yunnysunny <yunnysunny@gmail.com>
2018-03-05 22:11:13 +0800

    add more bench testcase

--- test/bench_lru_test.js
+++ test/bench_lru_test.js
@@ -1,11 +1,12 @@
 const {expect} = require('chai');
 const async = require('neo-async');
 const Redis = require('ioredis');
+const slogger = require('node-slogger');
 const SessionToken = require('../index');
 const redisClient = new Redis();//connect to the redis server of localhost:6379
 const redisSub = new Redis();//the redis client for subscribe
 const MAX_SIZE = 8192;
-const sessionToken = new SessionToken({
+const sessionTokenWithLru = new SessionToken({
     expireTime:7200,//the time of seconds before the session data expired
     redisKeyPrefix:'myprefix:mylrutoken:',//the redis key's prefix
     redis:redisClient,//the redis client object
@@ -14,8 +15,20 @@ const sessionToken = new SessionToken({
     useLru:true,
     //showMemSizeInterval:10*1000
 });
+
+const sessionTokenWithoutLru = new SessionToken({
+    expireTime:7200,//the time of seconds before the session data expired
+    redisKeyPrefix:'myprefix:mynolrutoken:',//the redis key's prefix
+    redis:redisClient,//the redis client object
+    subReis:redisSub,
+    maxSize:MAX_SIZE,
+    //showMemSizeInterval:10*1000
+});
+
 const VALUE = {name:'sunny',id:1};
-const LOOP_SIZE = 10240;
+const LOOP_SIZE = 102400;
+const LruToken = new Array(LOOP_SIZE);
+slogger.init({level:'warn'});
 
 describe('lru benchmark test',function() {
     it ('should generate '+LOOP_SIZE+'th tokens success',function(done) {
@@ -23,22 +36,47 @@ describe('lru benchmark test',function() {
             if (n > MAX_SIZE) {
                 // console.log('may overflow now');
             }
-            sessionToken.generate(VALUE,function(err/*,tokenViaCreate*/) {//save session
+            sessionTokenWithLru.generate(VALUE,function(err,tokenViaCreate) {//save session
                 if (err) {
                     return next(err);
                 }
-                
+                LruToken[n] = tokenViaCreate;
                 next();
             });
         },function(err) {
             if (err) {
                 return done(err);
             }
-            console.log(sessionToken._lruList.size(),sessionToken.data.size);
+            console.log(sessionTokenWithLru._lruList.size(),sessionTokenWithLru.data.size);
             //expect(sessionToken.data.size).to.be.lte(MAX_SIZE);
             
             done();
         });
         
+    });
+    it('remove all data create via lru session token',function(done) {
+        async.each(LruToken,function(token,next) {
+            sessionTokenWithLru.delete(token,next);
+        },done);
+    });
+    it('bench for none lur',function(done) {
+        async.times(LOOP_SIZE,function(n,next) {
+            sessionTokenWithoutLru.generate(VALUE,function(err,tokenViaCreate) {//save session
+                if (err) {
+                    return next(err);
+                }
+                LruToken[n] = tokenViaCreate;
+                next();
+            });
+        },done);
+    });
+    it('remove all data create via none lru session token',function(done) {
+        async.each(LruToken,function(token,next) {
+            sessionTokenWithoutLru.delete(token,next);
+        },done);
+    });
+    it('recovery log level',function(done) {
+        slogger.init({level:'trace'});
+        done();
     });
 });
